9369976c84701da90ef75adcbc39a339992d4848
he9mei <396167189@qq.com>
2019-12-16 20:48:47 +0800

    pytest-fixture

---
+++ pytest_leaning/pytest_leaning_07-1.py
@@ -0,0 +1,37 @@
+'''
+七、pytest-fixtue
+1.fixture优势
+fixture相对于setup和teardown来说应该有以下几点优势：
+（1）命名方式灵活，不局限于setup和teardown这几个命名。
+（2）conftest.py配置里可以实现数据共享，不需要import就能自动找到一些配置。
+（3）scope="module"可以实现多个.py跨文件共享前置，每一个.py文件调用一次。
+（4）scope="session"可以实现多个.py跨文件使用一个session来完成多个用例。
+使用：
+fixtue(scope="function",params=None,autouse=false,ids=None,name=None)
+  使用装饰器标记fixture的功能
+  可以使用此装饰器（带或不带参数）来定义fixture功能，fixture功能的名称可以在以后使用。
+  引用它会在测试之前调用它：test模块或类可以使用pytest.mark.usefixtures(fixture名称)
+  测试功能可以直接使用fixture名称作为输入参数。
+参数：
+  scope 有四个参数级别：function(默认)、class、module、session
+  params 一个可选的参数列表，它将导致多个参数调用fixture功能和所有测试使用它。
+  autouse 如果为true，则所有测试激活fixture；如果为false，则需要显示激活。
+  ids 每个字符串ID的列表，没有字符串对应与params；如果没有提供ID，它们将从params自动生成。
+  name fixture的名称。默认为装饰函数的名称。如果fixture在定义它的同一个模块中使用，夹具的功能名称将被请求夹具的功能arg遮蔽。
+  解决这个问题的一种方法就是将装饰函数命名。"fixture_(fixturename)"然后使用@pytest.fixture(name='(fixturename)')---不太懂
+
+2.fixture创建和使用
+fixture创建：
+(1) @pytest.fixture()
+(2) 编写一个普通函数
+@pytest.fixture()
+def login()
+    print("执行登录")
+
+fixture使用：
+在需要使用fixture的测试用例中，当作参数传入即可
+def test_shoping(login):
+    print("测试购物")
+
+实例：使用fixture实现setup和teardown
+'''
---
+++ pytest_leaning/pytest_leaning_07-2.py
@@ -0,0 +1,21 @@
+'''
+pytest-fixtue补充
+
+（1）fixture作用范围
+
+fixture里面有个scope参数可以控制fixture的作用范围:session > module > class > function
+- function 每一个函数或方法都会调用
+- class  每一个类调用一次，一个类可以有多个方法
+- module 每一个.py文件调用一次，该文件内又有多个function和class
+- session 是多个文件调用一次，可以跨.py文件调用，每个.py文件就是module
+
+scope=session
+fixture为session级别是可以跨.py模块调用的,也就是当我们有多个.py文件的用例时候，
+如果多个用例只需调用一次fixture，那就可以设置为scope="session"，并且写到conftest.py文件里
+conftest.py文件名称是固定的，pytest会自动识别该文件。
+放到工程的根目录下，就可以全局调用了，如果放到某个package包下，那就只在该package内有效
+
+参考：
+https://www.cnblogs.com/nuonuozhou/p/10429701.html
+
+'''
