6215bd503c39cf5648aa6d7b16203b5d5d5736c5
he9mei <396167189@qq.com>
2020-05-20 20:07:56 +0800

    po_appium优化-失败截图添加到allure报告

---
+++ learning_pytest/pytest_leaning_02.py
@@ -0,0 +1,78 @@
+'''
+二、用例执行
+1.在pycharm的Terminal执行用例：
+默认路径即当前项目，如rootdir: /Users/hehuaimei/PycharmProjects/python_appium
+补充：其他2种执行方式
+（1）也可以在cmd执行，先cd进入项目目录
+（2）py文件内执行，需要有__main_方法，见test_04
+（备注：测试用例可以写在类中，也可以不需要类直接写在函数中）
+如：
+if __name__=="__main__":
+    pytest.main("-s test_04.py")
+
+pycharm的Terminal执行命令如下：
+-->pytest
+当前路径下的用例全部执行
+learning_pytest/test_01.py ..                                                                                                       [ 50%]
+learning_pytest/test_02.py ..
+
+-->pyetst -s -m "test"
+针对标记执行某些用例
+配合使用的是在函数前加上@pytest.mark.test
+后面这个test是随便取得名字，识别不了，会报警。
+mark标记官方文档：https://docs.pytest.org/en/latest/mark.html
+报警解决办法：https://www.cnblogs.com/TestTan/p/11493177.html
+
+-->pytest learning_pytest/test_01.py
+执行指定路径/指定py文件
+如果不想执行某些类或者函数，可标记跳过，在该类或函数前加上：@pytest.mark.skip
+
+-->pytest learning_pytest/test_01.py::TestDemo
+执行指定路径/指定py文件::指定class
+
+-->pytest learning_pytest/test_01.py::TestDemo::test_test1
+执行指定路径/指定py文件::指定class::指定函数
+
+2.常用参数说明：
+-v：说明：可以输出用例更加详细的执行信息，比如用例所在的文件及用例名称等
+-s：说明：输入我们用例中的调式信息，比如print的打印信息等
+-m ：说明：执行特定的测试用例,”标记“
+-k： 说明：执行用例包含“关键字”的用例 ,"关键字"
+-q： 说明：简化控制台的输出
+--lf：当一次用例执行完成后，如果其中存在失败的测试用例，那么我们可以使用此命令重新运行失败的测试用例===很重要
+--ff：如果上次测试用例出现失败的用例，当使用--ff后，失败的测试用例会首先执行，剩余的用例也会再次执行一次
+进一步说明：
+-v
+加上-v可以看到每个函数的执行结果。如，
+pytest -v learning_pytest/test_01.py
+结果为：
+learning_pytest/test_01.py::TestDemo::test_test1 PASSED                                                                             [ 50%]
+learning_pytest/test_01.py::TestDemo::test_test2 PASSED
+其他的，如关键字k，失败重跑lf、ff后续再验证。==pytest --lf已经验证，可以重跑上次失败的用例集合。
+-m
+如果用例中包含多个分组，想要只运行其中一个组，则使用-m "组名"的方式。
+使用方式是，再函数前加上@pytest.mark.test (test是自己定义的名字，报警解决办法见前面的链接)
+执行时，使用-->pytest -m "test"
+-k
+k选项允许我们设置表达式来运行某些用例,表达式的写法有许多，
+可以用全称如test_case1这样也可以去掉test_，除了or外也可以使用not来指定那些用例不跑；
+执行时，使用-->pytest -v -k "test_01 or test_02" 即执行test_01和test_02两个py文件
+执行时，使用--> pytest -v -k "not test_02" 即除了test_02这个py文件不执行，其他都执行
+-x
+pytest的原本运行规则是每条用例均执行，不管是否有失败，如果我们想在用例运行时遇到失败即停止，则可以使用-x
+
+pytest其他用法，如
+-->pytest --help
+-->pytest --version
+
+3.执行结果
+绿色..表示成功；s表示跳过；F表示失败
+
+附加：
+关于pytest用例执行文档：
+https://www.cnblogs.com/test000/p/11613459.html
+https://blog.csdn.net/df0128/article/details/91043150
+
+
+'''
+
--- po_appium/conftest.py
+++ po_appium/conftest.py
@@ -1,11 +1,15 @@
-
+import os
+import time
+import allure
 import pytest
 from appium import webdriver
 from time import sleep
 import logging
 import logging.config
 import yagmail
+from po_appium.base.base_page import Base
 
+app_driver = None
 
 caps = {
     # "automationName": "Appium",
@@ -23,19 +27,21 @@ caps = {
 }
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="session", autouse=True)
 def driver(request):
-    driver = webdriver.Remote("http://localhost:4723/wd/hub",caps)
-    driver.implicitly_wait(10)
-    print("启动driver")
+    global app_driver
+    if app_driver is None:
+        app_driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+        app_driver.implicitly_wait(10)
+        print("启动driver")
 
     def end():
         sleep(5)
-        driver.quit()
+        app_driver.quit()
         print("关闭driver")
 
     request.addfinalizer(end)
-    return driver
+    return app_driver
 
 
 '''
@@ -94,3 +100,62 @@ def send_mail(attachment):
 
 # 每个py文件都重新赋值了logger，但是第二个py文件的log还是放在第一个py文件的py_name文件中。说明还是用的上一个py文件的logger
 # 尝试解决：每个py文件前面生成logger；最后再移除ogger。（移除fh处理器即可）
+# 这样也不行，最后的解决办法是：每个py文件生成的logger名字不同，使用了py_name
+
+
+# 失败截图方法
+def fail_capture():
+    dt=time.strftime("%Y-%m-%d",time.localtime(time.time()))
+    # tm=time.strftime("%Y-%m-%d_%H_%M_%S",time.localtime(time.time()))  #图片文件名加上日期和时间
+    tm=time.strftime("%H-%M-%S",time.localtime(time.time()))  #由于文件夹是以日期命名的，文件名省去日期
+    path=f"./test_result/fail_capture/{dt}"
+    try:
+        if not os.path.exists(path):
+            os.makedirs(path)
+        filename=path+"/"+"fail_"+tm+".png"
+        # print(f"失败截图方法中的driver是:{app_driver}")  # 确认此处的driver就是setup的driver
+        app_driver.get_screenshot_as_file(filename)  # 调用driver的截图方法
+
+        # 把失败截图放到allure中
+        with allure.step("添加失败截图"):
+            with open(filename, mode="rb") as f:
+                file = f.read()
+            allure.attach(file, "fail_capture", allure.attachment_type.PNG)
+            # 此处图片名字待改进，否则多张图时，不好区分
+
+    except Exception as e:
+        print(e)
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    '''
+    hook pytest失败
+    :param item:
+    :param call:
+    :return:
+    '''
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    rep = outcome.get_result()
+
+    # we only look at actual failing test calls, not setup/teardown
+    if rep.when == "call" and rep.failed:
+
+        path = "./test_result/"  # 我自己添加的failures文件的路径,默认放在主路径
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        mode = "a" if os.path.exists(f"{path}failures") else "w"
+        with open(f"{path}failures", mode) as f:
+            # let's also access a fixture for the fun of it
+            if "tmpdir" in item.fixturenames:
+                extra = " (%s)" % item.funcargs["tmpdir"]
+            else:
+                extra = ""
+            f.write(rep.nodeid + extra + "\n")
+        # fail_capture()  # 方式1：调用失败截图并加入allure的方法
+
+        # 方式2：直接使用以下方式也可以，直接把失败截图添加到allure（但是图片不会保存到本地）
+        with allure.step('添加失败截图'):
+            allure.attach(app_driver.get_screenshot_as_png(), "失败截图", allure.attachment_type.PNG)
